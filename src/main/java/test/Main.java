2cec7dc1086aaca9e66338ee74380af0c5b4d980
drityddv <1016653792@qq.com>
2019-04-26 10:52:02 +0800

    add slf4j-log4j support

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,9 @@
+package test;
+
+/**
+ * @author : ddv
+ * @since : 2019/4/24 下午2:39
+ */
+
+public class Main {
+}
---
+++ src/main/java/util/CollectionUtil.java
@@ -0,0 +1,9 @@
+package util;
+
+/**
+ * @author : ddv
+ * @since : 2019/4/25 上午9:53
+ */
+
+public class CollectionUtil {
+}
