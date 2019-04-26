c7cab61895b35bf04f3cb347e241e362979a15a5
drityddv <1016653792@qq.com>
2019-04-26 11:01:45 +0800

    add utils

--- src/main/java/util/CollectionUtil.java
+++ src/main/java/util/CollectionUtil.java
@@ -1,9 +1,23 @@
 package util;
 
+import java.util.Collection;
+
 /**
+ * 集合工具类
+ *
  * @author : ddv
  * @since : 2019/4/25 上午9:53
  */
 
 public class CollectionUtil {
+
+	/**
+	 * 容器是否为空
+	 *
+	 * @param collection
+	 * @return
+	 */
+	public static boolean isNotBlank(Collection collection) {
+		return collection != null && collection.size() > 0;
+	}
 }
