902c30a9f66275970be284c1fe9fdb0b99a3c268
drityddv <1016653792@qq.com>
2019-12-06 14:42:38 +0800

    分配网络,采用静态ip方式先处理容器通信,服务调用的问题

--- docker/dockerfile/build.sh
+++ docker/dockerfile/build.sh
@@ -8,5 +8,8 @@ cd /Users/ddv/workspace/java/miniServer/docker/dockerfile/mysql
 docker build -t dockerfile-mysql:v1 .
 cd /Users/ddv/workspace/java/miniServer/docker/dockerfile/redis
 docker build -t dockerfile-redis:v1 .
+cd /Users/ddv/workspace/java/miniServer/docker/dockerfile/centos
+docker build -t dockerfile-centos:v1 .
 cd /Users/ddv/workspace/java/miniServer
 docker build -t dockerfile-miniserver:v1 .
+
